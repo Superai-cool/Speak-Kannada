ac90453d0bdfb4336d06763d65611073053fabe5
Super AI <superai.india@gmail.com>
2025-05-05 22:56:49 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,15 +1,11 @@
 import os
-import json
 import firebase_admin
 from firebase_admin import credentials, db
+import json
 
-# Read JSON from environment variable
-firebase_json = os.environ.get("FIREBASE_JSON")
-if not firebase_json:
-    raise ValueError("Missing FIREBASE_JSON variable")
-
-cred = credentials.Certificate(json.loads(firebase_json))
+firebase_json = json.loads(os.environ.get("FIREBASE_JSON"))
+cred = credentials.Certificate(firebase_json)
 
 firebase_admin.initialize_app(cred, {
-    'databaseURL': os.environ.get("FIREBASE_DB_URL")
+    "databaseURL": os.environ.get("FIREBASE_DB_URL")
 })
